86eac2f24288cca1ed41f76427ff31873fa518d4
Krumbit <12988040+Krumbit@users.noreply.github.com>
2024-04-04 17:43:18 -0700

    style: formatting

--- src/app/api/clerk/route.ts
+++ src/app/api/clerk/route.ts
@@ -4,23 +4,23 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { Webhook } from "svix";
 
-const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || ``
+const webhookSecret = process.env.CLERK_WEBHOOK_SECRET || ``;
 
 async function validateRequest(request: Request) {
-  const payloadString = await request.text()
-  const headerPayload = headers()
+  const payloadString = await request.text();
+  const headerPayload = headers();
 
   const svixHeaders = {
-    'svix-id': headerPayload.get('svix-id')!,
-    'svix-timestamp': headerPayload.get('svix-timestamp')!,
-    'svix-signature': headerPayload.get('svix-signature')!,
-  }
-  const wh = new Webhook(webhookSecret)
-  return wh.verify(payloadString, svixHeaders) as WebhookEvent
+    "svix-id": headerPayload.get("svix-id")!,
+    "svix-timestamp": headerPayload.get("svix-timestamp")!,
+    "svix-signature": headerPayload.get("svix-signature")!,
+  };
+  const wh = new Webhook(webhookSecret);
+  return wh.verify(payloadString, svixHeaders) as WebhookEvent;
 }
 
 export async function POST(req: Request) {
-  const { type, data } = await validateRequest(req)
+  const { type, data } = await validateRequest(req);
 
   if (type === "user.created" && data.id) {
     await db.insert(users).values({ id: data.id, whitelisted: false });
