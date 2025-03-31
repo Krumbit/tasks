00d1b14cef8557379fa9d4f055199355d80c5718
Krumbit <12988040+Krumbit@users.noreply.github.com>
2025-03-30 18:45:16 -0700

    chore: inform of disabled app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,26 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button, Link } from "@nextui-org/react";
 
 export default function HomePage() {
+  // return (
+  //   <>
+  //     <SignedIn>
+  //       <Button color="primary" as={Link} href="/tasks">
+  //         Go to Tasks
+  //       </Button>
+  //     </SignedIn>
+  //     <SignedOut>
+  //       <div className="flex items-center justify-center gap-4">
+  //         <StyledSignIn />
+  //         <p>or</p>
+  //         <StyledSignUp />
+  //       </div>
+  //     </SignedOut>
+  //   </>
+  // );
+
   return (
-    <>
-      <SignedIn>
-        <Button color="primary" as={Link} href="/tasks">
-          Go to Tasks
-        </Button>
-      </SignedIn>
-      <SignedOut>
-        <div className="flex items-center justify-center gap-4">
-          <StyledSignIn />
-          <p>or</p>
-          <StyledSignUp />
-        </div>
-      </SignedOut>
-    </>
+    <span>
+      The hosted version of <strong>Tasks</strong> has been disabled.
+    </span>
   );
 }
